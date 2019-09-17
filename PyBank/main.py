5bb1385362c22c34a32462e49e678946d7fff949
Claudia Ahrens <claudia.s.ahrens@gmail.com>
2019-09-16 22:00:10 -0500

    printing out all rows of budget

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,6 +10,22 @@ greatest_increase_profit = 0
 greatest_decrease_date = ""
 greatest_decrease_profit = 0
 
+# parse csv file
+path =os.path.join(".","02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv")
+print(path)
+
+# open the file using "write" mode. Specify the variable to hold the contents
+with open(path, newline='') as csvfile:
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # skipping over column headers
+    next(csvreader)
+    
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
+
 
 print("Financial Analysis")
 print("----------------------------")
