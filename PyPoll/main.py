19718dd29ffa9118972a1b57f5e972e61f3b08dc
Claudia Ahrens <claudia.s.ahrens@gmail.com>
2019-09-16 21:29:38 -0500

    Updating PyPoll variables and printing

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,18 @@
 # PyPoll main for U of M assignment
+import os
+import csv
+
+total_votes = 0
+candidate_list = { }
+percent_votes_candidate_won = 0
+total_votes_candiate_won = 0
+winner_of_popular_vote = ""
+		
+print("Election Results")
+print("----------------------------")
+print('Total Votes: ' +str(total_votes))
+print("----------------------------")
+#Loop over dictionary of candidates and votes
+#print(candidate_list)': ' +str(percent_of_votes_candidate_won) + ' (' + str(total_votes_candiate_won) + ")")
+print("----------------------------")
+print('Winner: ' + winner_of_popular_vote)
